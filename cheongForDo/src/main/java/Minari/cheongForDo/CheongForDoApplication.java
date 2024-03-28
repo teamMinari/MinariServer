fa5c51f1f45b40bdea7e15d39e3ece676daf6718
SeongGeol <139744159+whitenoise13@users.noreply.github.com>
2024-03-28 17:18:29 +0900

    start
    시작합니다

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/CheongForDoApplication.java
@@ -0,0 +1,13 @@
+package Minari.cheongForDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongForDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongForDoApplication.class, args);
+	}
+
+}
---
+++ cheongForDo/src/test/java/Minari/cheongForDo/CheongForDoApplicationTests.java
@@ -0,0 +1,13 @@
+package Minari.cheongForDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheongForDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
