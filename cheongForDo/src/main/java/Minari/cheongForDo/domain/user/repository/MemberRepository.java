9d6938cc8299d65fb81b7729fc8f6763bd740f9b
whitenoise13 <amnesiamar13@gmail.com>
2024-04-11 17:09:56 +0900

    Feat :: MemberRepository 추가

--- cheongForDo/src/main/java/Minari/cheongForDo/domain/user/repository/MemberRepository.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/user/repository/MemberRepository.java
@@ -1,13 +1,13 @@
 package Minari.cheongForDo.domain.user.repository;
 
-import Minari.cheongForDo.domain.user.entity.Member;
+import Minari.cheongForDo.domain.member.entity.MemberEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
+    MemberEntity save(MemberEntity member);
+    Optional<MemberEntity> findById(Long id);
     Optional<java.lang.reflect.Member> findByName(String name);
     List<java.lang.reflect.Member> findAll();
 }
