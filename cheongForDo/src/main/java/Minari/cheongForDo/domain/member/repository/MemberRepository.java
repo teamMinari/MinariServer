e099e21c238c1fa85825758b62ff1b212ac5e46d
whitenoise13 <amnesiamar13@gmail.com>
2024-04-11 17:54:29 +0900

    Feat :: MemberRepository 추가

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package Minari.cheongForDo.domain.member.repository;
+
+import Minari.cheongForDo.domain.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+}
