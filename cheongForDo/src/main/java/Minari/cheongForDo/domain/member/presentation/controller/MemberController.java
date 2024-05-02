a3f0064c565d9310d025ce2cb20b77ed600a6c1e
whitenoise13 <amnesiamar13@gmail.com>
2024-05-02 21:02:52 +0900

    jwt코드 추가

--- cheongForDo/src/main/java/Minari/cheongForDo/domain/member/presentation/controller/MemberController.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/member/presentation/controller/MemberController.java
@@ -1,27 +1,12 @@
 package Minari.cheongForDo.domain.member.presentation.controller;
 
 import Minari.cheongForDo.domain.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import Minari.cheongForDo.domain.member.entity.MemberEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
 public class MemberController {
-    private final MemberService MEMBER_SERVICE;
 
-    @Autowired
-    public MemberController(MemberService MEMBER_SERVICE) {
-        this.MEMBER_SERVICE = MEMBER_SERVICE;
-    }
-
-    @PostMapping("/google-register")
-    public MemberEntity googleRegister(@RequestBody MemberEntity member) {
-        return MEMBER_SERVICE.registerWithGoogle(member);
-    }
-
-    @GetMapping("/user/{id}")
-    public MemberEntity getUserById(@PathVariable Long id) {
-        return MEMBER_SERVICE.findById(id);
-    }
 }
--- cheongForDo/src/main/java/Minari/cheongForDo/domain/member/repository/MemberRepository.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/member/repository/MemberRepository.java
@@ -2,8 +2,13 @@ package Minari.cheongForDo.domain.member.repository;
 
 import Minari.cheongForDo.domain.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    boolean existsById(String idx);
+    Optional<MemberEntity> findBy(String idx);
+
+    Optional<MemberEntity> findByEmail(String email);
 }
