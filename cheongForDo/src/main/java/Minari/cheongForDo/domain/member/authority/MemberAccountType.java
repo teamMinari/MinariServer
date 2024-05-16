eb02837f852aadd8470b57e93a1d194d11a9108e
whitenoise13 <amnesiamar13@gmail.com>
2024-05-16 18:04:44 +0900

    Feat :: MemberAccountType 구현

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/member/authority/MemberAccountType.java
@@ -0,0 +1,22 @@
+package Minari.cheongForDo.domain.member.authority;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public enum MemberAccountType {
+
+    ROLE_USER,
+    ROLE_ADMIN;
+
+    public static GrantedAuthority convert(MemberAccountType type) {
+        switch (type) {
+            case ROLE_USER :
+                return new SimpleGrantedAuthority("ROLE_USER");
+            case ROLE_ADMIN :
+                return new SimpleGrantedAuthority("ROLE_ADMIN");
+            default :
+                throw new IllegalArgumentException("잘못된 권한 이름");
+        }
+    }
+
+}
