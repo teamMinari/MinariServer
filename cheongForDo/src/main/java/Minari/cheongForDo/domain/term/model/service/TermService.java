25dc4f7380d73f168f6c5e4e885a3df9735a07c2
김호준 <hojoon4868@gmail.com>
2024-05-16 18:13:29 +0900

    Feat :: TermService 생성

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/term/model/service/TermService.java
@@ -0,0 +1,15 @@
+package Minari.cheongForDo.domain.term.model.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TermService {
+        private final TermService termService;
+
+        // 글 생성
+
+}
