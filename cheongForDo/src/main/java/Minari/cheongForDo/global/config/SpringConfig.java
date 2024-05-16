0b777a20e47ace4cabe5319040dd32a1b23e299f
whitenoise13 <amnesiamar13@gmail.com>
2024-05-16 18:06:58 +0900

    Feat :: SpringConfig 구현

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/global/config/SpringConfig.java
@@ -0,0 +1,27 @@
+package Minari.cheongForDo.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
