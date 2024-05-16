d05e4026e74a0a055e16c9e3fef3394f7580919e
김호준 <hojoon4868@gmail.com>
2024-05-16 18:13:12 +0900

    Feat :: TermResponseDTO 생성

---
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/term/dto/TermResponseDTO.java
@@ -0,0 +1,13 @@
+package Minari.cheongForDo.domain.term.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TermResponseDTO {
+    // 용어 이름
+
+}
