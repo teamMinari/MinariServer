ad48f7850bf3e1f9d9c2743b29fcec3f73d0f44f
whitenoise13 <amnesiamar13@gmail.com>
2024-05-16 18:05:01 +0900

    Fix :: MemberController 수정

--- cheongForDo/src/main/java/Minari/cheongForDo/domain/member/presentation/controller/MemberController.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/member/presentation/controller/MemberController.java
@@ -1,12 +1,31 @@
 package Minari.cheongForDo.domain.member.presentation.controller;
 
+import Minari.cheongForDo.domain.member.presentation.dto.MemberLoginDTO;
+import Minari.cheongForDo.domain.member.presentation.dto.MemberRegisterDTO;
 import Minari.cheongForDo.domain.member.service.MemberService;
+import Minari.cheongForDo.global.response.BaseResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("register")
+    public BaseResponse<?> postRegister(@Valid @RequestBody MemberRegisterDTO dto) {
+        return memberService.postRegister(dto);
+    }
+
+    @PostMapping("login")
+    public BaseResponse<?> postLogin(@Valid @RequestBody MemberLoginDTO dto) {
+        return memberService.postLogin(dto);
+    }
 
 }
