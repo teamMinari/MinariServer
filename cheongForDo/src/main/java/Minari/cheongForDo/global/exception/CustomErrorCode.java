08fa042e1766b5cc36d11a8e067ae0efb38b893c
김호준 <hojoon4868@gmail.com>
2024-06-05 23:05:24 +0900

    fix :: CustomErrorCode 수정

--- cheongForDo/src/main/java/Minari/cheongForDo/global/exception/CustomErrorCode.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/global/exception/CustomErrorCode.java
@@ -13,6 +13,9 @@ public enum CustomErrorCode {
     MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M-2", "멤버가 존재하지 않는다"),
     MEMBER_NOT_CORRECT(HttpStatus.BAD_REQUEST, "M-3", "멤버 정보가 일치하지 않다"),
 
+    // 용어 관련 오류
+    TERM_NOT_EXIST(HttpStatus.NOT_FOUND, "T-1", "용어가 존재하지 않음"),
+
     // JWT 관련 오류
     JWT_WAS_EXPIRED(HttpStatus.FORBIDDEN, "J-1", "토큰이 만료되었다");
 
