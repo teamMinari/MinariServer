bcc5133f99d44609ab41f370ed7493c560f29305
김호준 <hojoon4868@gmail.com>
2024-06-05 23:05:36 +0900

    fix :: Term 수정

--- cheongForDo/src/main/java/Minari/cheongForDo/domain/term/entity/Term.java
+++ cheongForDo/src/main/java/Minari/cheongForDo/domain/term/entity/Term.java
@@ -1,6 +1,8 @@
 package Minari.cheongForDo.domain.term.entity;
 
-import Minari.cheongForDo.domain.term.model.enums.TermyDifficulty;
+import Minari.cheongForDo.domain.term.dto.TermRequestDTO;
+import Minari.cheongForDo.domain.term.model.enums.TermCategory;
+import Minari.cheongForDo.domain.term.model.enums.TermDifficulty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -28,16 +30,39 @@ public class Term { // 추가로 들어가야 할 것 : 용어 유사어 termSiW
     // 용어 난이도
     @Column
     @Enumerated(EnumType.STRING)
-    private TermyDifficulty termyDifficulty;
+    private TermDifficulty termDifficulty;
+
+    // 용어 카테고리
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TermCategory termCategory;
+
+    // 용어 좋아요
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Boolean termLike;
 
     @Builder
     public Term(
             String termNm,
             String termExplain,
-            TermyDifficulty termyDifficulty
+            TermDifficulty termDifficulty,
+            TermCategory termCategory,
+            Boolean termLike
     ) {
         this.termNm = termNm;
         this.termExplain = termExplain;
-        this.termyDifficulty = termyDifficulty;
+        this.termDifficulty = termDifficulty;
+        this.termCategory = termCategory;
+        this.termLike = termLike;
     }
+
+    public void update(TermRequestDTO termRequestDTO) {
+        this.termNm = termRequestDTO.getTermNm();
+        this.termExplain = termRequestDTO.getTermExplain();
+        this.termDifficulty = termRequestDTO.getTermDifficulty();
+        this.termCategory = termRequestDTO.getTermCategory();
+        this.termLike = termRequestDTO.getTermLike();
+    }
+
 }
