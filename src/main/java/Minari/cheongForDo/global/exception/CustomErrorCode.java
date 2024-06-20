ccb9110231de841396b7bdb4e9bfe7f39f42d2f8
whitenoise13 <amnesiamar13@gmail.com>
2024-06-20 10:19:02 +0900

    FIX :: message 수정

--- src/main/java/Minari/cheongForDo/global/exception/CustomErrorCode.java
+++ src/main/java/Minari/cheongForDo/global/exception/CustomErrorCode.java
@@ -9,18 +9,18 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode {
 
     // 멤버 관련 오류
-    MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "M-1", "멤버가 이미 존재한다"),
-    MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M-2", "멤버가 존재하지 않는다"),
-    MEMBER_NOT_CORRECT(HttpStatus.BAD_REQUEST, "M-3", "멤버 정보가 일치하지 않다"),
+    MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "M-1", "멤버가 이미 존재합니다."),
+    MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M-2", "멤버가 존재하지 않습니다."),
+    MEMBER_NOT_CORRECT(HttpStatus.BAD_REQUEST, "M-3", "멤버 정보가 일치하지 않습니다."),
 
     // 용어 관련 오류
-    TERM_NOT_EXIST(HttpStatus.NOT_FOUND, "T-1", "용어가 존재하지 않음"),
+    TERM_NOT_EXIST(HttpStatus.NOT_FOUND, "T-1", "용어가 존재하지 않습니다."),
 
     // JWT 관련 오류
-    JWT_WAS_EXPIRED(HttpStatus.FORBIDDEN, "J-1", "토큰이 만료되었다"),
+    JWT_WAS_EXPIRED(HttpStatus.FORBIDDEN, "J-1", "토큰이 만료되었습니다."),
 
     // 퀴즈 관련 오류
-    QUESTION_NOT_EXIST(HttpStatus.NOT_FOUND, "Q-1", "질문이 존재하지 않음");
+    QUESTION_NOT_EXIST(HttpStatus.NOT_FOUND, "Q-1", "질문이 존재하지 않습니다.");
 
     private final HttpStatus code;
     private final String status;
