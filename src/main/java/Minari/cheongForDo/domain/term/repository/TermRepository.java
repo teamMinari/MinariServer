9dc4809797d0beb8b7fb7f62a26601ef7f7f6729
김호준 <hojoon4868@gmail.com>
2024-07-08 20:23:05 +0900

    Fix :: TermRepository 수정

--- src/main/java/Minari/cheongForDo/domain/term/repository/TermRepository.java
+++ src/main/java/Minari/cheongForDo/domain/term/repository/TermRepository.java
@@ -3,9 +3,12 @@ package Minari.cheongForDo.domain.term.repository;
 import Minari.cheongForDo.domain.term.entity.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface TermRepository extends JpaRepository<Term, String> {
 
     List<Term> findAllByTermNm(String termNm);
