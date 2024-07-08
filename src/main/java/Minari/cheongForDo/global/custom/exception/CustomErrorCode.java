bc8ddfacf9505dc4b8aef827366f61874dd26408
whitenoise13 <amnesiamar13@gmail.com>
2024-07-08 19:19:57 +0900

    Fix :: 서버 오류 예외 처리, 비밀번호 오류 예외 처리 추가

--- src/main/java/Minari/cheongForDo/global/custom/exception/CustomErrorCode.java
+++ src/main/java/Minari/cheongForDo/global/custom/exception/CustomErrorCode.java
@@ -1,4 +1,4 @@
-package Minari.cheongForDo.global.exception;
+package Minari.cheongForDo.global.custom.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CustomErrorCode {
+    // 서버 오류
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-1", "서버에서 오류가 발생했습니다."),
 
     // 멤버 관련 오류
     MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "M-1", "멤버가 이미 존재합니다."),
     MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M-2", "멤버가 존재하지 않습니다."),
     MEMBER_NOT_CORRECT(HttpStatus.BAD_REQUEST, "M-3", "멤버 정보가 일치하지 않습니다."),
+    PASSWORDS_DO_NOT_MATCH(HttpStatus.BAD_REQUEST, "M-4", "비밀번호가 일치하지 않습니다."),
 
     // 용어 관련 오류
     TERM_NOT_EXIST(HttpStatus.NOT_FOUND, "T-1", "용어가 존재하지 않습니다."),
