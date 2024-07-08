7069f931edf4e41a7d049550177d3f30e3668941
whitenoise13 <amnesiamar13@gmail.com>
2024-07-08 19:16:04 +0900

    Feat :: AdminOnly 추가

---
+++ src/main/java/Minari/cheongForDo/global/custom/annotation/AdminOnly.java
@@ -0,0 +1,15 @@
+package Minari.cheongForDo.global.custom.annotation;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public @interface AdminOnly {
+}
