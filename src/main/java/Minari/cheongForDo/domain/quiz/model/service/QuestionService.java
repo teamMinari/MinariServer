19a44873ba55f63df968d9e354bb87f9de4494cf
김호준 <hojoon4868@gmail.com>
2024-07-10 17:04:27 +0900

    Fix :: QuestionService 수정

--- src/main/java/Minari/cheongForDo/domain/quiz/model/service/QuestionService.java
+++ src/main/java/Minari/cheongForDo/domain/quiz/model/service/QuestionService.java
@@ -1,31 +1,52 @@
-package Minari.cheongForDo.domain.quiz.service;
+package Minari.cheongForDo.domain.quiz.model.service;
 
 import Minari.cheongForDo.domain.quiz.dto.QuestionRequestDTO;
+import Minari.cheongForDo.domain.quiz.dto.QuestionResponseDTO;
 import Minari.cheongForDo.domain.quiz.entity.Question;
 import Minari.cheongForDo.domain.quiz.repository.QuestionRepository;
+import Minari.cheongForDo.domain.term.dto.TermResponseDTO;
 import Minari.cheongForDo.global.exception.CustomException;
 import Minari.cheongForDo.global.response.BaseResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import static Minari.cheongForDo.global.exception.CustomErrorCode.QUESTION_NOT_EXIST;
+
+
+/*
+"quiz": {
+    "questions": [1, 2, 3, 4, ...]
+} // NoSQL (Mongodb, redis, ...etc) <<
+
+QZ   QT   QZ-QT
+1    1-10  1-1 1-2 1-3 1-4 ... 1-10 // Relational (MySQL, MariaDB, ...etc)
+* */
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class QuestionService { // 질문 일일, 전체 조회 필요합니다.
+public class QuestionService { //
 
     private final QuestionRepository questionRepository;
+
+    // 질문 전체 조회
+    public List<QuestionResponseDTO> getQuestions() {
+        List<Question> questionList = questionRepository.findAll();
+        return questionList.stream().map(
+                QuestionResponseDTO::of
+        ).toList();
+    }
 
     // 질문 생성
     public BaseResponse<?> createQuestion(QuestionRequestDTO createDTO) {
         Question question = Question.builder()
-                .qtName(createDTO.getQtName())
                 .qtContents(createDTO.getQtContents())
                 .qtAnswer(createDTO.getQtAnswer())
                 .qtCmt(createDTO.getQtCmt())
-                .qtTerm(createDTO.getQtTerm())
+                .qtDifficulty(createDTO.getQtDifficulty())
                 .build();
 
         questionRepository.save(question);
@@ -36,6 +57,14 @@ public class QuestionService { // 질문 일일, 전체 조회 필요합니다.
                 "질문 생성 성공 !!",
                 null
         );
+    }
+
+    // 질문 하나 조회
+    public QuestionResponseDTO findOneQuestion(Long qtIdx) {
+        Question question = questionRepository.findById(qtIdx).orElseThrow(
+                () -> new CustomException(QUESTION_NOT_EXIST)
+        );
+        return QuestionResponseDTO.of(question);
     }
 
     // 질문 수정
