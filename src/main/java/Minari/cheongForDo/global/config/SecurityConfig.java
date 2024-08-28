531a6527d51602f4046bd9ce29113deb82c4dbc3
whitenoise13 <amnesiamar13@gmail.com>
2024-08-28 09:39:03 +0900

    Fix :: cors 에러 수정

--- src/main/java/Minari/cheongForDo/global/config/SecurityConfig.java
+++ src/main/java/Minari/cheongForDo/global/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 허용할 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList("*")); // 허용할 도메인 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
