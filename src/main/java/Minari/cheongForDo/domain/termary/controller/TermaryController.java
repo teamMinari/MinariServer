fb7b0b225ef8b558b61f58b3d0ccf6648e7825d1
whitenoise13 <amnesiamar13@gmail.com>
2024-09-05 23:39:12 +0900

    Fix:: GetMapping으로 요청할 수 있도록 수정

--- src/main/java/Minari/cheongForDo/domain/termary/controller/TermaryController.java
+++ src/main/java/Minari/cheongForDo/domain/termary/controller/TermaryController.java
@@ -2,20 +2,20 @@ package Minari.cheongForDo.domain.termary.controller;
 
 import Minari.cheongForDo.domain.termary.service.TermaryService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import Minari.cheongForDo.domain.termary.dto.TermaryRequestDTO;
 
 @RequestMapping("/termary")
 @RestController
 @AllArgsConstructor
 public class TermaryController {
+
     private final TermaryService termaryService;
 
-    @PostMapping("/summarize")
-    public String summarizeTerm(@RequestBody TermaryRequestDTO termaryRequestDTO) {
+    @GetMapping("/summarize")
+    public String summarizeTerm(@RequestParam String termNm) {
+        TermaryRequestDTO termaryRequestDTO = new TermaryRequestDTO();
+        termaryRequestDTO.setTermNm(termNm);
         return termaryService.summarizeTerm(termaryRequestDTO);
     }
 }
