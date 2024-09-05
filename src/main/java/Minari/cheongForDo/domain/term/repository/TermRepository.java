4b8eb70545c72c3634c11b228b660c75d2ae4898
whitenoise13 <amnesiamar13@gmail.com>
2024-09-05 23:45:30 +0900

    Fix:: develop branch와 동일하게 설정

--- src/main/java/Minari/cheongForDo/domain/term/repository/TermRepository.java
+++ src/main/java/Minari/cheongForDo/domain/term/repository/TermRepository.java
@@ -3,16 +3,16 @@ package Minari.cheongForDo.domain.term.repository;
 import Minari.cheongForDo.domain.term.entity.Term;
 import Minari.cheongForDo.domain.term.model.enums.TermDifficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TermRepository extends JpaRepository<Term, Long> {
 
     List<Term> findAllByTermDifficulty(TermDifficulty level);
-
+    Page<Term> findAll(Pageable pageable);
+    Term findByTermNm(String termNm);
 
 }
