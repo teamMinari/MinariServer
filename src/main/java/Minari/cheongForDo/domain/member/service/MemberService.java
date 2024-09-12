62bcee7a59f1da888af6e0d6b0898343cb7e9fac
whitenoise13 <amnesiamar13@gmail.com>
2024-09-12 18:57:41 +0900

    Fix:: 출석체크 로직 추가

--- src/main/java/Minari/cheongForDo/domain/member/service/MemberService.java
+++ src/main/java/Minari/cheongForDo/domain/member/service/MemberService.java
@@ -26,9 +26,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
-
-import static Minari.cheongForDo.global.exception.CustomErrorCode.*;
 
 @Service
 @Transactional
