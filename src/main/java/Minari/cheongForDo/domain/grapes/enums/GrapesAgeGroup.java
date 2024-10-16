c64d1da35fddfd832242925e1201f58c6e242bf1
김호준 <hojoon4868@gmail.com>
2024-10-16 16:44:05 +0900

    Feat :: 포도송이 나이 & 연령대 카테고리 추가

---
+++ src/main/java/Minari/cheongForDo/domain/grapes/enums/GrapesAgeGroup.java
@@ -0,0 +1,9 @@
+package Minari.cheongForDo.domain.grapes.enums;
+
+public enum GrapesAgeGroup {
+    TEENS,
+    TWENTIES,
+    THIRTIES,
+    FORTIES,
+    FIFTIES
+}
--- src/main/java/Minari/cheongForDo/domain/grapes/enums/GrapesCategory.java
+++
@@ -1,9 +0,0 @@
-package Minari.cheongForDo.domain.grapes.enums;
-
-public enum GrapesCategory {
-    BEGINNER, // 초급
-    INTERMEDIATE, // 중급
-    ADVANCED, // 고급
-    START, // 첫 시작
-    ENCOMICSYSTEM // 경제 제도
-}
